fb2a15c3ae2c4aa2d21dd9c70d879b7fccc53942
AlanThinks <a@alanthinks.com>
2018-03-06 19:26:51 -0500

    "commiting latest master"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './custom.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <h1>React Contact Manager</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form id="contact-form">
+
+    <div className="messages"></div>
+
+    <div className="controls">
+
+        <div className="row">
+            <div className="col-md-6">
+                <div className="form-group">
+                    <label name="form_name">Firstname *</label>
+                    <input id="form_name" type="text" name="name" className="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required."/>
+                    <div className="help-block with-errors"></div>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <div className="form-group">
+                    <label name="form_lastname">Lastname *</label>
+                    <input id="form_lastname" type="text" name="surname" className="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required."/>
+                    <div className="help-block with-errors"></div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-6">
+                <div className="form-group">
+                    <label name="form_email">Email *</label>
+                    <input id="form_email" type="email" name="email" className="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required."/>
+                    <div className="help-block with-errors"></div>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <div className="form-group">
+                    <label name="form_phone">Phone</label>
+                    <input id="form_phone" type="tel" name="phone" className="form-control" placeholder="Please enter your phone"/>
+                    <div className="help-block with-errors"></div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-12">
+                <div className="form-group">
+                    <label name="form_message">Message *</label>
+                    <textarea id="form_message" name="message" className="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+                    <div className="help-block with-errors"></div>
+                </div>
+            </div>
+            <div className="col-md-12">
+                <input type="submit" className="btn btn-success btn-send" value="Send message"/>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-12">
+                <p className="text-muted"><strong>*</strong> These fields are required</p>
+            </div>
+        </div>
+    </div>
+
+</form>
       </div>
     );
   }
